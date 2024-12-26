d0de8b4cf1facd7d7a378c251313ae5c3f78c74b
TAUREAN312 <jbotchwayowusu@gmail.com>
2024-12-26 21:09:01 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,185 +1,173 @@
 --Request for input 
 while true do
-  print([[
-  [1] Area 
-  [2] Perimeter 
-  [3] Volume]])
+  print("\27[34m \nSELECT ONE\27[0m")
+  print("\27[32m [1] Area\n [2] Perimeter\n [3] Volume\27[0m\n")
   local input = io.read()
 
 --Area
   if input == "1" then
-    print([[Which Plane Figure 
-    [1] Square
-    [2] Rectangle
-    [3] Triangle
-    [4] Circle]])
+    print("\27[34m \nWHICH PLANE FIGURE\27[0m")
+    print("\27[32m [1] Square\n [2] Rectangle\n [3] Triangle\n [4] Circle\27[0m\n")
     local shape=io.read()
   
     --Area Of Square
   if shape == "1" then
-    print("Enter Length:") 
+    print("\27[34m\nEnter Length:\27[0m") 
     local length=io.read()
-    print("Enter S.I unit:")
+    print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
-    print("The Area Of The Square Is", length*length ..unit.. "²")
+    print("\27[33m\nThe Area Of The Square Is\27[0m", length*length ..unit.. "\27[33m²\27[0m")
   
   --Area Of Rectangle
   elseif shape == "2" then
-    print("Enter Length:") 
+    print("\27[34m\nEnter Length:\27[0m") 
     local length=io.read()
-    print("Enter Width:")
+    print("\27[34m\nEnter Width:\27[0m")
     local width=io.read()
-    print("Enter S.I unit:")
+    print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
-    print("The Area Of The Rectangle Is", length*width ..unit.. "²")
+    print("\27[33m\nThe Area Of The Rectangle Is\27[0m", length*width ..unit.. "\27[33m²\27[0m")
   
   --Area Of Triangle
   elseif shape == "3" then
-    print("Enter Perpendicular Height:")
+    print("\27[34m\nEnter Perpendicular Height:\27[0m")
     local ph=io.read()
-    print("Enter Length Of Base:")
+    print("\27[34m\nEnter Length Of Base:\27[0m")
     local base=io.read()
-    print("Enter S.I unit:")
+    print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
-    print("Area Of The Triangle Is", (1/2)*base*ph ..unit.. "²")
+    print("\27[33m\nArea Of The Triangle Is\27[0m", (1/2)*base*ph ..unit.. "\27[33m²\27[0m")
   
   --Area Of Circle
   elseif shape == "4" then
-    print("Enter Radius:")
+    print("\27[34m\nEnter Radius:\27[0m")
     local radius=io.read()
     local pi=3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
-    print("Enter S.I unit:")
+    print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
-    print("The Area Of The Circle Is", pi*radius*radius ..unit.. "²")
+    print("\27[33m\nThe Area Of The Circle Is\27[0m", pi*radius*radius ..unit.. "\27[33m²\27[0m\n")
 
-  else print("Error:Invalid Input")
+  else print("\27[31m\nError:Invalid Input\27[31m\n")
 end
 end  
   
   --Perimeter
   if input == "2" then
-      print([[Which Plane Figure 
-    [1] Square
-    [2] Rectangle]])
+      print("\27[34mWHICH PLANE FIGURE\27[0m")
+    print("\27[32m [1] Square\n [2] Rectangle\27[0m\n")
      local figure=io.read()
   
     --Perimeter Of Square
   if figure == "1" then
-    print("Enter Length:") 
+    print("\27[34m\nEnter Length:\27[0m") 
     local length=io.read()
-    print("Enter S.I unit")
+    print("\27[34m\nEnter S.I unit\27[0m")
     local unit=io.read()
-    print("The Perimeter Of The Square Is", length+length+length+length ..unit)
+    print("\27[33m\nThe Perimeter Of The Square Is\27[0m", length+length+length+length ..unit)
   
   --Perimeter Of Rectangle
   elseif figure == "2" then
-    print("Enter Length:") 
+    print("\27[34m\nEnter Length:\27[0m") 
     local length=io.read()
-    print("Enter Width")
+    print("\27[34m\nEnter Width\27[0m")
     local width=io.read()
-    print("Enter S.I unit")
+    print("\27[34m\nEnter S.I unit\27[0m")
     local unit=io.read()
-    print("The Perimeter Of The Rectangle Is", length+length+width+width ..unit)
+    print("\27[33m\nThe Perimeter Of The Rectangle Is\27[0m", length+length+width+width ..unit)
   
   else 
-    print("Error:Invalid Input")
+    print("\27[31m\nError:Invalid Input\27[0m\n")
 end
 end
 
   --Volume
   if input == "3" then
-    print([[Which 3d Object 
-    [1] Cube
-    [2] Cuboid
-    [3] Cone
-    [4] Cylinder 
-    [5] Sphere
-    [6] Pyramid]])  
+    print("\27[34m\nWHICH 3D OBJECT\27[0m") 
+    print("\27[32m [1] Cube\n [2] Cuboid\n [3] Cone\n [4] Cylinder\n [5] Sphere\n [6] Pyramid\27[0m\n")  
     local object=io.read()
   
   --Volume Of Cube
   if object == "1" then
-    print("Enter Length:") 
+    print("\27[34m\nEnter Length:\27[0m") 
     local length=io.read()
-    print("Enter S.I unit:")
+    print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
-    print("The Volume Of The Cube Is", length*length*length ..unit.. "³")
+    print("\27[33m\nThe Volume Of The Cube Is\27[0m", length*length*length ..unit.. "\27[33m³\27[0m")
   
   --Volume Of Cuboid 
   elseif object == "2" then
-    print("Enter Length:") 
+    print("\27[34m\nEnter Length:\27[0m") 
     local length=io.read()
-    print("Enter Width:")
+    print("\27[34m\nEnter Width:\27[0m")
     local width=io.read()
-    print("Enter Height:")
+    print("\27[34m\nEnter Height:\27[0m")
     local height=io.read()
-    print("Enter S.I unit:")
+    print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
-    print("The Volume Of The Cuboid Is", length*width*height ..unit.. "³")
+    print("\27[33m\nThe Volume Of The Cuboid Is\27[0m", length*width*height ..unit.. "\27[33m³\27[0m")
   
   --Volume Of Cone
   elseif object == "3" then
-    print("Enter Perpendicular Height:")
+    print("\27[34m\nEnter Perpendicular Height:\27[0m")
     local pheight=io.read()
-    print("Enter Radius Of Its Circular Base:")
+    print("\27[34m\nEnter Radius Of Its Circular Base:\27[0m")
     local radius=io.read()
-    print("Enter S.I unit:")
+    print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
     local pi=3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
-    print("The Volume Of The Cone Is", (1/3)*pi*radius*radius*pheight ..unit.. "³")
+    print("\27[33m\nThe Volume Of The Cone Is\27[0m", (1/3)*pi*radius*radius*pheight ..unit.. "\27[33m³\27[0m")
 
   --Volume Of Cylinder
   elseif object == "4" then
-    print("Enter Radius:")
+    print("\27[34m\nEnter Radius:\27[0m")
     local radius=io.read()
-    print("Enter Height Of Cylinder:")
+    print("\27[34m\nEnter Height Of Cylinder:\27[0m")
     local height=io.read()
     local pi=3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
-    print("Enter S.I unit:")
+    print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
-    print("The Volume Of The Cylinder Is", pi*radius*radius*height ..unit.. "³")
+    print("\27[33m\nThe Volume Of The Cylinder Is\27[0m", pi*radius*radius*height ..unit.. "\27[33m³\27[0m")
     
   --Volume Of Sphere
   elseif object == "5" then
-    print("Enter Radius:") 
+    print("\27[34m\nEnter Radius:\27[0m") 
     local radius=io.read()
     local pi=3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
-    print("Enter S.I unit:")
+    print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
-    print("The Volume Of The Sphere Is", (4/3)*pi*radius*radius*radius ..unit.. "³")
+    print("\27[33m\nThe Volume Of The Sphere Is\27[0m", (4/3)*pi*radius*radius*radius ..unit.. "\27[33m³\27[0m")
   
   --Volume Of Pyramid
   
   elseif object == "6" then
-    print([[What Type Of Pyramid Is It 
-      [1] Square
-      [2] Triangular]])
+    print("\27[34m\nWHICH PYRAMID\27[0m") 
+    print("\27[32m [1] Square\n [2] Triangular\27[0m")
     local pyramid=io.read()
   
   --Volume Of Square Pyramid
   if pyramid == "1" then
-    print("Enter Length Of Base:") 
+    print("\27[34m\nEnter Length Of Base:\27[0m") 
     local length=io.read()
-    print("Enter Perpendicular Height Of Pyramid:")
+    print("\27[34m\nEnter Perpendicular Height Of Pyramid:\27[0m")
     local pheight=io.read()
-    print("Enter S.I unit:")
+    print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
-    print("The Volume Of The Square Pyramid Is", (1/3)*length*length*pheight ..unit.. "³")
+    print("\27[33m\nThe Volume Of The Square Pyramid Is\27[0m", (1/3)*length*length*pheight ..unit.. "\27[33m³\27[0m")
   
   --Volume Of Triangular Pyramid
   elseif pyramid == "2" then
-    print("Enter Length Of Base:") 
+    print("\27[34m\nEnter Length Of Base:\27[0m") 
     local length=io.read()
-    print("Enter Perpendicular Height Of Base(Triangle):")
+    print("\27[34m\nEnter Perpendicular Height Of Base (Triangle):\27[0m")
     local pbase=io.read()
-    print("Enter Perpendicular Height Of Pyramid:")
+    print("\27[34m\nEnter Perpendicular Height Of Pyramid:\27[0m")
     local pheight=io.read()
-    print("Enter S.I unit:")
+    print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
-    print("The Volume Of The Triangular Pyramid Is", (1/3)*(1/2)*pbase*length*pheight ..unit.. "³")
+    print("\27[33m\nThe Volume Of The Triangular Pyramid Is\27[0m", (1/3)*(1/2)*pbase*length*pheight ..unit.. "\27[33m³\27[0m")
   
   else
-    print("Error:Invalid Input")  
+    print("\27[31m\n\nError:Invalid Input\n\27[0m")  
 end
 end
 end
