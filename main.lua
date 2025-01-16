cd802649118def87ba3311e1472cfa5c9a461687
TAUREAN312 <jbotchwayowusu@gmail.com>
2025-01-16 07:46:54 +0000

    vap_calc.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,14 @@
+
 --Request for input 
 while true do
+  print([[
+ __        __   _         ____
+ \ \      / /  / \ \    |  __ \  
+  \ \    / /  / __\ \   |  ___/
+   \ \  / /  / /   \ \  | |
+    \__/ /  /_/     \_\ |_|
+]])
+  
   print("\27[34m \nSELECT ONE\27[0m")
   print("\27[32m [1] Area\n [2] Perimeter\n [3] Volume\27[0m\n")
   local input = io.read()
@@ -16,6 +25,7 @@ while true do
     local length=io.read()
     print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
+    os.execute("clear")
     print("\27[33m\nThe Area Of The Square Is\27[0m", length*length ..unit.. "\27[33m²\27[0m")
   
   --Area Of Rectangle
@@ -26,8 +36,10 @@ while true do
     local width=io.read()
     print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
+    os.execute("clear")
     print("\27[33m\nThe Area Of The Rectangle Is\27[0m", length*width ..unit.. "\27[33m²\27[0m")
-  
+    
+
   --Area Of Triangle
   elseif shape == "3" then
     print("\27[34m\nEnter Perpendicular Height:\27[0m")
@@ -36,8 +48,10 @@ while true do
     local base=io.read()
     print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
+    os.execute("clear")
     print("\27[33m\nArea Of The Triangle Is\27[0m", (1/2)*base*ph ..unit.. "\27[33m²\27[0m")
   
+
   --Area Of Circle
   elseif shape == "4" then
     print("\27[34m\nEnter Radius:\27[0m")
@@ -45,9 +59,10 @@ while true do
     local pi=3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
     print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
+    os.execute("clear")
     print("\27[33m\nThe Area Of The Circle Is\27[0m", pi*radius*radius ..unit.. "\27[33m²\27[0m\n")
 
-  else print("\27[31m\nError:Invalid Input\27[31m\n")
+  else print("\27[31m\nError:Invalid Input\27[30m\n")
 end
 end  
   
@@ -63,8 +78,9 @@ end
     local length=io.read()
     print("\27[34m\nEnter S.I unit\27[0m")
     local unit=io.read()
+    os.execute("clear")
     print("\27[33m\nThe Perimeter Of The Square Is\27[0m", length+length+length+length ..unit)
-  
+ 
   --Perimeter Of Rectangle
   elseif figure == "2" then
     print("\27[34m\nEnter Length:\27[0m") 
@@ -73,8 +89,10 @@ end
     local width=io.read()
     print("\27[34m\nEnter S.I unit\27[0m")
     local unit=io.read()
-    print("\27[33m\nThe Perimeter Of The Rectangle Is\27[0m", length+length+width+width ..unit)
-  
+    os.execute("clear") 
+   print("\27[33m\nThe Perimeter Of The Rectangle Is\27[0m", length+length+width+width ..unit)
+ 
+
   else 
     print("\27[31m\nError:Invalid Input\27[0m\n")
 end
@@ -92,8 +110,9 @@ end
     local length=io.read()
     print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
+    os.execute("clear")
     print("\27[33m\nThe Volume Of The Cube Is\27[0m", length*length*length ..unit.. "\27[33m³\27[0m")
-  
+ 
   --Volume Of Cuboid 
   elseif object == "2" then
     print("\27[34m\nEnter Length:\27[0m") 
@@ -104,8 +123,9 @@ end
     local height=io.read()
     print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
-    print("\27[33m\nThe Volume Of The Cuboid Is\27[0m", length*width*height ..unit.. "\27[33m³\27[0m")
-  
+    os.execute("clear") 
+   print("\27[33m\nThe Volume Of The Cuboid Is\27[0m", length*width*height ..unit.. "\27[33m³\27[0m")
+ 
   --Volume Of Cone
   elseif object == "3" then
     print("\27[34m\nEnter Perpendicular Height:\27[0m")
@@ -115,7 +135,8 @@ end
     print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
     local pi=3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
-    print("\27[33m\nThe Volume Of The Cone Is\27[0m", (1/3)*pi*radius*radius*pheight ..unit.. "\27[33m³\27[0m")
+    os.execute("clear") 
+   print("\27[33m\nThe Volume Of The Cone Is\27[0m", (1/3)*pi*radius*radius*pheight ..unit.. "\27[33m³\27[0m")
 
   --Volume Of Cylinder
   elseif object == "4" then
@@ -126,8 +147,9 @@ end
     local pi=3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
     print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
+    os.execute("clear")
     print("\27[33m\nThe Volume Of The Cylinder Is\27[0m", pi*radius*radius*height ..unit.. "\27[33m³\27[0m")
-    
+   
   --Volume Of Sphere
   elseif object == "5" then
     print("\27[34m\nEnter Radius:\27[0m") 
@@ -135,8 +157,9 @@ end
     local pi=3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
     print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
+    os.execute("clear")
     print("\27[33m\nThe Volume Of The Sphere Is\27[0m", (4/3)*pi*radius*radius*radius ..unit.. "\27[33m³\27[0m")
-  
+ 
   --Volume Of Pyramid
   
   elseif object == "6" then
@@ -152,6 +175,7 @@ end
     local pheight=io.read()
     print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
+    os.execute("clear")
     print("\27[33m\nThe Volume Of The Square Pyramid Is\27[0m", (1/3)*length*length*pheight ..unit.. "\27[33m³\27[0m")
   
   --Volume Of Triangular Pyramid
@@ -164,6 +188,7 @@ end
     local pheight=io.read()
     print("\27[34m\nEnter S.I unit:\27[0m")
     local unit=io.read()
+    os.execute("clear")
     print("\27[33m\nThe Volume Of The Triangular Pyramid Is\27[0m", (1/3)*(1/2)*pbase*length*pheight ..unit.. "\27[33m³\27[0m")
   
   else
